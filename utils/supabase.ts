041cf9b1c8f9558593ee744a928ac8e0de5c2471
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-19 06:43:21 -0700

    Update supabase.ts

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,7 +1,17 @@
-import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.https://nyatadsfrhtbfxfnyldv.supabase.co;
-const supabaseKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55YXRhZHNmcmh0YmZ4Zm55bGR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwNjY2NzYsImV4cCI6MjA2MDY0MjY3Nn0.xzUU-pmZlrTO-6-TzQu6M3eM_SeSTdjDNIHEiRtPj3Y;
+import 'react-native-url-polyfill/auto'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  process.env.EXPO_PUBLIC_SUPABASE_URL || "",
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "",
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  })
         
