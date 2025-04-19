84a8b17dc331ec2d6c80277cec14dc33aa7cb9eb
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-19 06:30:01 -0700

    Update server.ts

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,24 +1,21 @@
-import { createServerClient } from "@supabase/ssr";
+
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export const createClient = async () => {
-  const cookieStore = await cookies();
-
+export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.https://nyatadsfrhtbfxfnyldv.supabase.co!,
+    process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55YXRhZHNmcmh0YmZ4Zm55bGR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwNjY2NzYsImV4cCI6MjA2MDY0MjY3Nn0.xzUU-pmZlrTO-6-TzQu6M3eM_SeSTdjDNIHEiRtPj3Y!,
     {
       cookies: {
         getAll() {
-          return cookieStore.getAll();
+          return cookieStore.getAll()
         },
         setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) => {
-              cookieStore.set(name, value, options);
-            });
-          } catch (error) {
-            // The `set` method was called from a Server Component.
+            cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
+          } catch {
+            // The `setAll` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
             // user sessions.
           }
